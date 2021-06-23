a197cae4f5b31d24d5bc20633e11e284e215766f
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-06-23 19:33:21 -0300

    exercicio 9 ok

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
@@ -122,3 +122,12 @@ if(isParA%2==0||isParB%2==0||isParC%2==0){
 }
 
 //exercicio 9:
+let isImparA = 1;
+let isImparB = 130;
+let isImparC = 150;
+
+if(isImparA%2==1||isImparB%2==1||isImparC%2==1){
+    console.log(true);
+}else{
+    console.log(false)
+}
