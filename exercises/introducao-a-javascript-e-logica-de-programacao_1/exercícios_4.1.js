07c1a2db9d20a22a13a0e97ea461d36d92ea745e
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-06-23 19:55:59 -0300

    exercicio 10 ok

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
@@ -131,3 +131,16 @@ if(isImparA%2==1||isImparB%2==1||isImparC%2==1){
 }else{
     console.log(false)
 }
+
+//exercicio 10:
+let valorDeVenda = 8;
+let produto = 5;
+let montanteDeVenda = 1000;
+const valorDeCusto = produto *1.2;
+const lucro = valorDeVenda - valorDeCusto;
+
+if(valorDeVenda<0 || produto<0){
+    console.log('Error!')
+}else{
+    console.log('Seu lucro foi de: '+lucro*montanteDeVenda);
+}
