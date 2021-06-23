0272938fef06946b757a29b334e9f34d35c1e86e
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-06-23 17:50:44 -0300

    exercicio 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
@@ -26,3 +26,17 @@ if(maiorA>maiorB){
 else{
     console.log(maiorB);
 }
+
+//exercício 3:
+let trioA = 9;
+let trioB = 6;
+let trioC = 7;
+
+if(trioA>trioB && trioA>trioC){
+    console.log(trioA);
+}else if(trioB>trioC){
+    console.log(trioB);
+}
+else{
+    console.log(trioC);
+}
