93b97791101a3985c7e77b7354038b6f77be50c7
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-06-23 17:43:16 -0300

    exercicios 1 ao 2 ok

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
@@ -0,0 +1,28 @@
+//exercicio 1:
+let a = 5
+let b = 6
+//1:Adição
+let soma = a + b;
+console.log(soma);
+//1:Subtração
+let subtracao = a - b;
+console.log(subtracao);
+//1:Multiplicação
+let multiplicacao = a * b;
+console.log(multiplicacao);
+//1:Divisão
+let divisao = a / b;
+console.log(divisao);
+//1:Modulo
+let modulo = a % b;
+console.log(modulo);
+
+//exercicio 2:
+let maiorA = 10;
+let maiorB = 200;
+if(maiorA>maiorB){
+    console.log(maiorA);
+}
+else{
+    console.log(maiorB);
+}
