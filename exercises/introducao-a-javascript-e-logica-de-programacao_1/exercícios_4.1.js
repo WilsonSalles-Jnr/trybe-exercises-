6ac52f44268ef0d7e331c91bd8f05e24d447aa9c
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-06-23 19:17:25 -0300

    exercicio 8 (faltando o 6)

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
@@ -98,3 +98,15 @@ if(nota >100 || nota <0){
 }
 
 //exercicio 8:
+
+let isParA = 1;
+let isParB = 130;
+let isParC = 15;
+
+if(isParA%2==0||isParB%2==0||isParC%2==0){
+    console.log(true);
+}else{
+    console.log(false)
+}
+
+//exercicio 9:
