ee56b21ff61a6fdc65bb56bfc16d20252afc4ba3
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-06-23 18:21:56 -0300

    exercicio 5 ok

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercícios_4.1.js
@@ -52,3 +52,26 @@ else if(positivo<0){
 else{
     console.log('zero');
 }
+
+//exercicio 5:
+let angleA = -10;
+let angleB = 60;
+let angleC = 45;
+
+let somaAngulo = angleA + angleB + angleC;
+let triangulo;
+
+if(angleA < 0 || angleB < 0 || angleC < 0 || somaAngulo>180){
+    if(somaAngulo>180){
+        triangulo = false;
+        console.log('Fatal error! The sum of angles is more than 180º');
+        console.log('triangle is '+triangulo);
+    }else{
+        triangulo = false;
+        console.log('Fatal error! Angles is less than 0');
+        console.log('triangle is '+triangulo);
+    }
+}else{
+    triangulo = true;
+    console.log('triangle is '+triangulo);
+}
