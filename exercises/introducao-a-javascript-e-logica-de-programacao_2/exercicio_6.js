793b94161380deacc8452d8a241944902089dd7f
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-06-24 18:13:18 -0300

    exercicio 6 ok

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_6.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_6.js
@@ -1,1 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let valoresImpares =[];
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i]%2==1){
+        valoresImpares.push(numbers[i]);
+    }
+}
+console.log(valoresImpares)
