a833755e499bf940ed545bb14af44532aeaeb716
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-06-25 19:46:46 -0300

    exercicio 3 melhorad

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio_3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio_3.js
@@ -1,10 +1,12 @@
 let n = 5;
-let estrela = '*';
-let arr = ['','','','',''];
-let count = n-1;
-console.log(arr)
-for(let i = 0; i < n; i++){
+let estrela = '*'
+let arr = []
+let count = n-1
+for(let i = 0; i<n; i++){
+    arr.push('');
+}
+for(let j = 0; j<n; j++){
     arr[count]=estrela;
     count--;
-    console.log(arr)
+    console.log(arr);
 }
