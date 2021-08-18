941c0b4933f4d15f64bada3c3fc15fe9bba9b205
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-08-18 19:09:54 -0300

    exercicio 2 ok

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/Playground1a5.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/Playground1a5.js
@@ -1,85 +1,52 @@
-
-function compareTrue(bol1, bol2) {
-  if (bol1 === true && bol2 === true) {
-    return true;
-  }
-  return false;
-}
-
-
-function calcArea(base, height) {
-  let area = (base * height) / 2;
-  return area;
-}
-
-function splitSentence(frase) {
-  let arr = [];
-  let palavra = '';
-  for (let i = 0; i < frase.length; i += 1) {
-    if (frase[i] === ' ') {
-      arr.push(palavra);
-      palavra = '';
-    }
-    if (frase[i] !== ' ') {
-      palavra += frase[i];
-    }
-  }
-  arr.push(palavra);
-  return arr;
-}
-
-function concatName(arr) {
-  let primeiraPalavra = arr[0];
-  let ultimaPalavra = '';
-  for (let i = 0; i < arr.length; i += 1) {
-    ultimaPalavra = arr[i];
-  }
-  let ultimaPrimeira = `${ultimaPalavra}, ${primeiraPalavra}`;
-  return ultimaPrimeira;
-}
-
-
-function footballPoints(wins, ties) {
-  let pontos = wins * 3 + ties;
-  return pontos;
-}
 function encode(string) {
-    let code = [1, 2, 3, 4, 5];
-    let letra = ['a', 'e', 'i', 'o', 'u'];
-    let letraFinder = '';
-    let deploy = '';
-    for (let i = 0; i < string.length; i += 1) {
-      letraFinder = string[i];
-      let indexLetra = letra.indexOf(string[i]);
-      if (string[i] === letra[indexLetra]) {
-        let index = letra.indexOf(letraFinder);
-        deploy += code[index];
-      } else if (string[i] !== letra[indexLetra]) {
-        deploy += string[i];
-      }
+  let code = [1, 2, 3, 4, 5];
+  let letra = ['a', 'e', 'i', 'o', 'u'];
+  let letraFinder = '';
+  let deploy = '';
+  for (let i = 0; i < string.length; i += 1) {
+    letraFinder = string[i];
+    let indexLetra = letra.indexOf(string[i]);
+    if (string[i] === letra[indexLetra]) {
+      let index = letra.indexOf(letraFinder);
+      deploy += code[index];
+    } else if (string[i] !== letra[indexLetra]) {
+      deploy += string[i];
     }
-    return (deploy);
-}
-function decode(string) {
-    let letra = ['1', '2', '3', '4', '5'];
-    let code = ['a', 'e', 'i', 'o', 'u'];
-    let deploy = '';
-    for (let i = 0; i < string.length; i += 1) {
-      let indexLetra = letra.indexOf(string[i]);
-      if (string[i] === letra[indexLetra]) {
-        deploy += code[indexLetra];
-      } else if (string[i] !== letra[indexLetra]) {
-        deploy += string[i];
-      }
-    }
-    return deploy;
   }
+  return (deploy);
+}
+
+function decode(string) {
+  let letra = ['1', '2', '3', '4', '5'];
+  let code = ['a', 'e', 'i', 'o', 'u'];
+  let deploy = '';
+  for (let i = 0; i < string.length; i += 1) {
+    let indexLetra = letra.indexOf(string[i]);
+    if (string[i] === letra[indexLetra]) {
+      deploy += code[indexLetra];
+    } else if (string[i] !== letra[indexLetra]) {
+      deploy += string[i];
+    }
+  }
+  return deploy;
+}
+
+function techList(arr, name) {
+  if (arr.length === 0) {
+    return 'Vazio!';
+  }
+  arr.sort();
+  let tec = [];
+  arr.forEach((cur) => {
+    tec.push({tech: cur, name: name})
+  });
+  arr = tec
+  return arr.sort();
+
+}
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'))
 module.exports = {
-    compareTrue,
-    calcArea,
-    splitSentence,
-    concatName,
-    footballPoints,
-    encode,
-    decode
+  encode,
+  decode,
+  techList
 }
--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/Playground1a5.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/Playground1a5.test.js
@@ -1,27 +1,63 @@
-
-const {compareTrue,
-    calcArea,
-    splitSentence,
-    concatName,
-    footballPoints,
+const {
     encode,
-    decode
+    decode,
+    techList
 } = require('./Playground1a5');
 
-test('Teste se encode e decode são funções',()=>{
-    expect(typeof encode).toBe('function')
+
+describe('exercício 1:', () => {
+
+    test('Teste se encode e decode são funções', () => {
+        expect(typeof encode).toBe('function')
+    })
+
+    test('teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente', () => {
+        expect(encode('a, e, i, o, u')).toBe('1, 2, 3, 4, 5');
+    })
+    test('teste se os números 1, 2, 3, 4 e 5 são convertido nas vogais a, e, i, o, u , respectivamente', () => {
+        expect(decode('1,2,3,4,5')).toBe('a,e,i,o,u')
+    })
+    test('Teste se as demais letras/números não são convertidos para cada caso', () => {
+        expect(encode('macaco')).toBe('m1c1c4');
+        expect(decode('m1c1c4')).toBe('macaco')
+    })
+    test('Teste se a string que é retornada pelas funções têm o mesmo número de caracteres que a string passada como parâmetro.', () => {
+        expect(encode('param').length).toBe('param'.length)
+    })
 })
 
-test('teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente',()=>{
-    expect(encode('a, e, i, o, u')).toBe('1, 2, 3, 4, 5');
-})
-test('teste se os números 1, 2, 3, 4 e 5 são convertido nas vogais a, e, i, o, u , respectivamente',()=>{
-    expect(decode('1,2,3,4,5')).toBe('a,e,i,o,u')
-})
-test('Teste se as demais letras/números não são convertidos para cada caso',()=>{
-    expect(encode('macaco')).toBe('m1c1c4');
-    expect(decode('m1c1c4')).toBe('macaco')
-})
-test('Teste se a string que é retornada pelas funções têm o mesmo número de caracteres que a string passada como parâmetro.',()=>{
-    expect(encode('param').length).toBe('param'.length)
-})
+describe('Testa a função techList', () => {
+    it('Testa se a função techList é definida', () => {
+      expect(techList).toBeDefined();
+    });
+    it('Testa se techList é uma função', () => {
+      expect(typeof techList).toBe('function');
+    });
+    it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+      expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+        {
+          tech: 'CSS',
+          name: 'Lucas'
+        },
+        {
+          tech: 'HTML',
+          name: 'Lucas'
+        },
+        {
+          tech: 'JavaScript',
+          name: 'Lucas'
+        },
+        {
+          tech: 'Jest',
+          name: 'Lucas'
+        },
+        {
+          tech: 'React',
+          name: 'Lucas'
+        }
+      ]);
+    });
+    it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+      expect(techList([], 'Lucas')).toBe('Vazio!');
+    });
+  });
