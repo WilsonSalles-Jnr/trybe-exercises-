d4142048fc601d02050a1ddbd257a0d673ac2f17
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-09-03 15:33:11 -0300

    concluido

--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/ListarPokemons.js
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/ListarPokemons.js
@@ -5,9 +5,12 @@ class ListPokemon extends Component {
   render() { 
     const { data }=this.props;
     return (
+      <>
+        <h1>Pokedex</h1>
       <section className='pokecontainer'>
       {data.map((cur)=> <Pokedex key={cur.id} proper={cur}/>)}
       </section>
+      </>
      );
   }
 }
