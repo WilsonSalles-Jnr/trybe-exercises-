1c5e9f1403e331587f5bdd42e442f2f0f3b38b5d
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2022-01-07 19:44:14 -0300

    jogar novamente implementado

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/sorteio.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/sorteio.js
@@ -1,7 +1,14 @@
 const input = require('readline-sync');
 
 
-const sorteio = (number) => {
-  return number === input.questionInt('Digite um número de 1 a 10: ') ? 'Parabéns, número correto!' : `Opa, não foi dessa vez. O número era ${number}`
+const sorteio = () => {
+  let numero = Math.round(Math.random()*10);
+  console.log(numero === input.questionInt('Digite um número de 1 a 10: ')
+  ? 'Parabéns, número correto!'
+  : `Opa, não foi dessa vez. O número era ${numero}`)
+
+  return input.question("Quer jogar novamente? (y/n) ") === "y"
+  && sorteio()
+
 }
-console.log(sorteio(Math.round(Math.random()*10)))
+sorteio()
