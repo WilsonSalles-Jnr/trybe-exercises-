4b5d115c364ec0bc15be761a1dd6848edba3bb68
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2022-01-21 19:08:32 -0300

    resolvido

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/controllers/productController.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const ProductModel = require('../models/productModel');
+
+const router = express.Router();
+
+
+router.get('/list-products', async (req, res, next) => {
+  const products = await ProductModel.getAll();
+  res.status(200).json(products);
+});
+
+router.get('/get-by-id/:id', async (req, res, next) => {
+  const product = await ProductModel.getById(req.params.id);
+
+  res.status(200).json(product);
+});
+
+router.post('/add-user', async (req, res) => {
+  const { name, brand } = req.body;
+
+  const newProduct = await ProductModel.add(name, brand);
+
+  res.status(201).json(newProduct);
+});
+
+router.delete('/delete-user/:id', async (req, res) => {
+  const products = await ProductModel.exclude(req.params.id);
+
+  res.status(200).json(products);
+});
+
+router.put('/update-user/:id', async (req, res) => {
+  const { name, brand } = req.body;
+
+  const products = await ProductModel.update(req.params.id, name, brand);
+
+  res.status(200).json(products);
+});
+
+module.exports = router;
